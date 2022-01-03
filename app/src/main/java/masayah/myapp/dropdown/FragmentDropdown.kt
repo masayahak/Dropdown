b7036d7f726cd8d7bbc4830e812408436e4ef976
sin6032 <sin6032@live.jp>
2022-01-04 06:24:39 +0900

    20220104 微修正

--- app/src/main/java/masayah/myapp/dropdown/FragmentDropdown.kt
+++ app/src/main/java/masayah/myapp/dropdown/FragmentDropdown.kt
@@ -1,6 +1,5 @@
 package masayah.myapp.dropdown
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -22,7 +21,7 @@ class FragmentDropdown : Fragment() {
 
         // ドロップダウンの初期設定
         val dropdown : AutoCompleteTextView = view?.findViewById(R.id.dropdown_item)!!
-        initDropdown(requireContext(), dropdown)
+        initDropdown(dropdown)
 
     }
 
@@ -45,7 +44,7 @@ class FragmentDropdown : Fragment() {
         buttonSetDropdown.setOnClickListener {
             // サンプルなので固定で「洋食」をセット。
             // 本来はDBからの取得値などで値をセット。
-            setDropdownSelection(requireContext(), dropdown, "洋食")
+            setDropdownSelection(dropdown, "洋食")
         }
 
         // ドロップダウンの現在値をプログラムで取得するサンプル
@@ -59,19 +58,19 @@ class FragmentDropdown : Fragment() {
     }
 
     // ドロップダウンの初期設定
-    private fun initDropdown(content : Context, dropdown: AutoCompleteTextView) {
+    private fun initDropdown(dropdown: AutoCompleteTextView) {
         // 「R.array.foodType_array」は「values/array.xml」で定義
         val foodTypes = resources.getStringArray(R.array.foodType_array)
         // 「R.layout.dropdown_item」は「vlayout/dropdown_item.xml」で定義
-        val arrayAdapter = ArrayAdapter(content, R.layout.dropdown_item, foodTypes)
+        val arrayAdapter = ArrayAdapter(dropdown.rootView.context, R.layout.dropdown_item, foodTypes)
         dropdown.setAdapter(arrayAdapter)
     }
 
     // 特定の値をドロップダウンにセット
-    private fun setDropdownSelection(content : Context, dropdown: AutoCompleteTextView, item : String) {
+    private fun setDropdownSelection(dropdown: AutoCompleteTextView, item : String) {
         dropdown.setText(item)
         // setTextを実行するとドロップダウンへセットしたアダプタがリセットされるので再設定する
-        initDropdown(content, dropdown)
+        initDropdown(dropdown)
         // アダプタ再設定時にドロップダウンが開いた状態になるのを防ぐためにフォーカスを外す
         dropdown.clearFocus()
     }
